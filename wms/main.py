3c5af95541d4a31b973492554dac4519d7ced834
Genesis Visbal <sarayvisbal18@gmail.com>
2026-03-10 15:35:14 -0500

    abre cors temporalmente

--- wms/main.py
+++ wms/main.py
@@ -33,11 +33,7 @@ app = FastAPI(title="WMS API")
 # ==============================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://inova-delta.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
